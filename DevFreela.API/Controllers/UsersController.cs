399582b30dddd1af506dec61a31df2ffef23396b
Marcos Paulo <marcos.poo@gmail.com>
2025-01-27 17:51:44 -0300

    feat: Estrutura inicial do projeto.

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // POST api/users
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+    }
+}
