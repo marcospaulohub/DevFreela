ed0790d7655576f5c39497ff156edd2ead970632
Marcos Paulo <marcos.poo@gmail.com>
2025-01-28 19:50:56 -0300

    feat: Adicionando os Models.

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,7 +1,5 @@
 ﻿using DevFreela.API.Models;
-using DevFreela.API.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 
 namespace DevFreela.API.Controllers
 {
@@ -9,28 +7,21 @@ namespace DevFreela.API.Controllers
     [ApiController]
     public class ProjectsController : ControllerBase
     {
-        private readonly FreelanceTotalCostConfig _config;
-        private readonly IConfigService _configService;
-        public ProjectsController(
-            IOptions<FreelanceTotalCostConfig> options,
-            IConfigService configService)
+        public ProjectsController()
         {
-            _config = options.Value;
-            _configService = configService;
+ 
         }
         // GET api/projects?search=crm
         [HttpGet]
         public IActionResult Get(string search = "")
         {
-            return Ok(_configService.GetValue());
+            return Ok();
         }
 
         // GET api/projects/1234
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            throw new Exception();
-
             return Ok();
         }
 
@@ -38,11 +29,6 @@ namespace DevFreela.API.Controllers
         [HttpPost]
         public IActionResult Post(CreateProjectInputModel model)
         {
-            if(model.TotalCost < _config.Minimum || model.TotalCost > _config.Maximum)
-            {
-                return BadRequest("Numero fora dos limites.");
-            }
-
             return CreatedAtAction(nameof(GetById), new { id = 1 }, model);
         }
 
