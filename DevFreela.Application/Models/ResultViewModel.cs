c01cef85940bdd214639fd7395e9683c4f7f5857
Marcos Paulo <marcos.poo@gmail.com>
2025-01-29 12:28:46 -0300

    feat: Atualizando o Objeto ResultViewModel

--- DevFreela.Application/Models/ResultViewModel.cs
+++ DevFreela.Application/Models/ResultViewModel.cs
@@ -10,6 +10,12 @@
 
         public bool IsSuccess { get; private set; }
         public string Message { get; private set; }
+
+        public static ResultViewModel Success()
+            => new();
+
+        public static ResultViewModel Error(string message)
+            => new(false, message);
     }
 
     public class ResultViewModel<T> : ResultViewModel 
