3cb13b355d01e7a8db676e01e1250bb3c58ec5d8
Marcos Paulo <marcos.poo@gmail.com>
2025-01-29 11:30:56 -0300

    feat: Criando Objeto base referente ao padrão ResultViewModel.

---
+++ DevFreela.Application/Models/ResultViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace DevFreela.Application.Models
+{
+    public class ResultViewModel
+    {
+        public ResultViewModel(bool isSuccess = true, string message = "")
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+
+        public bool IsSuccess { get; private set; }
+        public string Message { get; private set; }
+    }
+
+    public class ResultViewModel<T> : ResultViewModel
+    {
+        public ResultViewModel(T data, bool isSuccess = true, string message = "")
+            : base(isSuccess, message) 
+        {
+            Data = data;
+        }
+
+        public T Data { get; private set; }
+    }
+
+}
