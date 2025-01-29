95f62bc180c9ef3de7626f59334e40d6c701e58d
Marcos Paulo <marcos.poo@gmail.com>
2025-01-29 12:41:18 -0300

    feat: Implementando a Camada de Aplicação

--- DevFreela.Application/Models/UpdateProjectInputModel.cs
+++ DevFreela.Application/Models/UpdateProjectInputModel.cs
@@ -2,6 +2,7 @@
 {
     public class UpdateProjectInputModel
     {
+        public int IdProject { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public decimal TotalCost { get; set; }
