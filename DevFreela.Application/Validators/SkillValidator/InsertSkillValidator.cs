155a2d6893e4b1fdb0dc19f3188dbb36828151fc
Marcos Paulo <marcos.poo@gmail.com>
2025-01-31 16:58:18 -0300

    feat: Implementando Validações com Fluent Validation no objeto Skill.

---
+++ DevFreela.Application/Validators/SkillValidator/InsertSkillValidator.cs
@@ -0,0 +1,17 @@
+﻿using DevFreela.Application.Commands.Skills.InsertSkill;
+using FluentValidation;
+
+namespace DevFreela.Application.Validators.SkillValidator
+{
+    public class InsertSkillValidator : AbstractValidator<InsertSkillCommand>
+    {
+        public InsertSkillValidator()
+        {
+            RuleFor(s => s.Description)
+                .NotEmpty()
+                    .WithMessage("Não pode ser vazio.")
+                .MaximumLength(80)
+                    .WithMessage("Tamanho máximo é 80 caracteres.");
+        }
+    }
+}
