97e7f41f89e3361b16f99244fbb2fd94b2a3fe70
Marcos Paulo <marcos.poo@gmail.com>
2025-01-31 16:57:09 -0300

    feat: Implementando Validações com Fluent Validation no objeto Project.
    FluentValidation na versão 11.3.0

---
+++ DevFreela.Application/Validators/ProjectValidator/InsertProjectValidator.cs
@@ -0,0 +1,35 @@
+﻿using DevFreela.Application.Commands.Projects.InsertProject;
+using FluentValidation;
+
+namespace DevFreela.Application.Validators.ProjectValidator
+{
+    public class InsertProjectValidator : AbstractValidator<InsertProjectCommand>
+    {
+        public InsertProjectValidator()
+        {
+            RuleFor(p => p.Title)
+                .NotEmpty()
+                    .WithMessage("Não pode ser vazio.")
+                .MaximumLength(100)
+                    .WithMessage("Tamanho máximo é 100 caracteres.");
+
+            RuleFor(p => p.Description)
+                .NotEmpty()
+                    .WithMessage("Não pode ser vazio.");
+
+            RuleFor(p => p.IdClient)
+                .NotEmpty()
+                    .WithMessage("Não pode ser vazio.");
+
+            RuleFor(p => p.IdFreelancer)
+                .NotEmpty()
+                    .WithMessage("Não pode ser vazio.");
+
+            RuleFor(p => p.TotalCost)
+                .NotEmpty()
+                    .WithMessage("Não pode ser vazio.")
+                .GreaterThanOrEqualTo(1000)
+                    .WithMessage("O projeto deve custar pelo menos R$1.000");
+        }
+    }
+}
