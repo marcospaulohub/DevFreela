49bf409403fa971637d67f2d28490bb882b62326
Marcos Paulo <marcos.poo@gmail.com>
2025-01-31 16:58:03 -0300

    feat: Implementando Validações com Fluent Validation no objeto User.

---
+++ DevFreela.Application/Validators/UserValidator/CreateUserValidator.cs
@@ -0,0 +1,31 @@
+﻿using DevFreela.Application.Models;
+using FluentValidation;
+
+namespace DevFreela.Application.Validators.UserValidator
+{
+    public class CreateUserValidator : AbstractValidator<CreateUserInputModel>
+    {
+        public CreateUserValidator()
+        {
+            RuleFor(u => u.FullName)
+                .NotEmpty()
+                    .WithMessage("Não pode ser vazio.")
+                .MaximumLength(200)
+                    .WithMessage("Tamanho máximo é 200 caracteres.");
+
+            RuleFor(u => u.Email)
+                .NotEmpty()
+                    .WithMessage("Não pode ser vazio.")
+                .MaximumLength(200)
+                    .WithMessage("Tamanho máximo é 200 caracteres.")
+                .EmailAddress()
+                    .WithMessage("E-mail inválido.");
+
+            RuleFor(u => u.BirthDate)
+                .NotEmpty()
+                    .WithMessage("Não pode ser vazio.")
+                .Must(d => d < DateTime.Now.AddYears(-18))
+                    .WithMessage("Deve ser maior de idade.");
+        }
+    }
+}
