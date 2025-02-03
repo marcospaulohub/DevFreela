458f5bf10882cf9696d395dfb7644e4bf1d5c723
Marcos Paulo <marcos.poo@gmail.com>
2025-02-02 21:54:58 -0300

    feat: Implementa Repository para a entidade User.

---
+++ DevFreela.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using DevFreela.Core.Entities;
+
+namespace DevFreela.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetById(int id);
+        Task<int> Add(User user);
+        Task AddSkill(UserSkill userSkills);
+    }
+}
