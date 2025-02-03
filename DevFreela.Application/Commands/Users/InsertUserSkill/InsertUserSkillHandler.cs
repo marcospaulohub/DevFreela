7adc48e78787ad5640ca6baac256b2123d002292
Marcos Paulo <marcos.poo@gmail.com>
2025-02-03 00:27:56 -0300

    feat: Implementa o padrão CQRS para o objeto User.

---
+++ DevFreela.Application/Commands/Users/InsertUserSkill/InsertUserSkillHandler.cs
@@ -0,0 +1,26 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using MediatR;
+
+namespace DevFreela.Application.Commands.Users.InsertUserSkill
+{
+    public class InsertUserSkillHandler : IRequestHandler<InsertUserSkillCommand, ResultViewModel>
+    {
+        private readonly IUserRepository _repository;
+
+        public InsertUserSkillHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ResultViewModel> Handle(InsertUserSkillCommand request, CancellationToken cancellationToken)
+        {
+            var userSkills = request.SkillIds.Select(s => new UserSkill(request.Id, s)).ToList();
+
+            await _repository.AddSkill(userSkills);
+
+            return ResultViewModel.Success();
+        }
+    }
+}
---
+++ DevFreela.Application/Queries/Users/GetUserById/GetUserByIdHandler.cs
@@ -0,0 +1,29 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Messages.UserMessages;
+using DevFreela.Core.Repositories;
+using MediatR;
+
+namespace DevFreela.Application.Queries.Users.GetUserById
+{
+    public class GetUserByIdHandler : IRequestHandler<GetUserByIdQuery, ResultViewModel<UserViewModel>>
+    {
+        private readonly IUserRepository _repository;
+
+        public GetUserByIdHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ResultViewModel<UserViewModel>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _repository.GetById(request.Id);
+
+            if (user is null)
+                return ResultViewModel<UserViewModel>.Error(UserMsgs.GetUserNotExist());
+
+            var model = UserViewModel.FromEntity(user);
+
+            return ResultViewModel<UserViewModel>.Success(model);
+        }
+    }
+}
