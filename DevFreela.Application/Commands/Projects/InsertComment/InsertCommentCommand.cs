8d68bfcfb492a83619d35aee8328fb1bc193925e
Marcos Paulo <marcos.poo@gmail.com>
2025-02-07 11:16:42 -0300

    test: Implementando teste no InsertCommentHandler.

--- DevFreela.Application/Commands/Projects/InsertComment/InsertCommentCommand.cs
+++ DevFreela.Application/Commands/Projects/InsertComment/InsertCommentCommand.cs
@@ -5,6 +5,13 @@ namespace DevFreela.Application.Commands.Projects.InsertComment
 {
     public class InsertCommentCommand : IRequest<ResultViewModel>
     {
+        public InsertCommentCommand(string content, int idProject, int idUser)
+        {
+            Content = content;
+            IdProject = idProject;
+            IdUser = idUser;
+        }
+
         public string Content { get; set; }
         public int IdProject { get; set; }
         public int IdUser { get; set; }
---
+++ DevFreela.UnitTests/Application/InsertCommentHandlerTests.cs
@@ -0,0 +1,29 @@
+﻿using DevFreela.Application.Commands.Projects.InsertComment;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Messages.ProjectMessages;
+using DevFreela.Core.Repositories;
+using NSubstitute;
+
+namespace DevFreela.UnitTests.Application
+{
+    public class InsertCommentHandlerTests
+    {
+        [Fact]
+        public async Task ProjectDoesNotExist_Error_NSubstitute()
+        {
+            // Arrange
+            var repository = Substitute.For<IProjectRepository>();
+            repository.AddComment(Arg.Any<ProjectComment>()).Returns(Task.FromResult);
+
+            var command = new InsertCommentCommand("Comentário", 1, 1);
+            var handler = new InsertCommentHandler(repository);
+
+            // Act
+            var result = await handler.Handle(command, new CancellationToken());
+
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal(ProjectMsgs.GetProjectNotExist(), result.Message);
+        }
+    }
+}
