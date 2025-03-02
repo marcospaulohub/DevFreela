f6ecb8de13f836908922c50a7a3d32a420275564
Marcos Paulo <marcos.poo@gmail.com>
2025-03-02 13:49:45 -0300

    test: Adiciona novos métodos para gerar dados Fakes.

--- DevFreela.UnitTests/Fakes/FakeDataHelper.cs
+++ DevFreela.UnitTests/Fakes/FakeDataHelper.cs
@@ -20,7 +20,7 @@ namespace DevFreela.UnitTests.Fakes
                 );
         }
 
-        private static readonly Faker<Project> _projectFaker = new Faker<Project>()
+        private static readonly Faker<Project> _projectFakerV1 = new Faker<Project>()
             .CustomInstantiator(f => new Project(
                 f.Commerce.ProductName(),
                 f.Lorem.Sentence(),
@@ -34,6 +34,34 @@ namespace DevFreela.UnitTests.Fakes
             .RuleFor(c => c.IdClient, f => f.Random.Int(1, 100))
             .RuleFor(c => c.IdFreelancer, f => f.Random.Int(1, 100))
             .RuleFor(c => c.TotalCost, f => f.Random.Decimal(1000, 10000));
+
+        private static readonly Faker<User> _userFakerClient = new Faker<User>()
+            .RuleFor(u => u.Id, f => f.Random.Int(1, 1000))
+            .RuleFor(u => u.FullName, f => f.Name.FullName())
+            .RuleFor(u => u.Email, f => f.Internet.Email())
+            .RuleFor(u => u.BirthDate, DateTime.Now.AddYears(-20))
+            .RuleFor(u => u.Password, f => f.Internet.Password(10))
+            .RuleFor(u => u.Role, "client");
+
+        private static readonly Faker<User> _userFakerFreelancer = new Faker<User>()
+            .RuleFor(u => u.Id, f => f.Random.Int(1, 1000))
+            .RuleFor(u => u.FullName, f => f.Name.FullName())
+            .RuleFor(u => u.Email, f => f.Internet.Email())
+            .RuleFor(u => u.BirthDate, DateTime.Now.AddYears(-20))
+            .RuleFor(u => u.Password, f => f.Internet.Password(10))
+            .RuleFor(u => u.Role, "freelancer");
+
+        private static readonly Faker<Project> _projectFaker = new Faker<Project>()
+            .RuleFor(p => p.Id, f => f.Random.Int(1, 1000))
+            .RuleFor(p => p.Title, f => f.Commerce.ProductName())
+            .RuleFor(p => p.Description, f => f.Lorem.Sentence())
+            .RuleFor(p => p.IdClient, f => f.Random.Int(1, 100))
+            .RuleFor(p => p.IdFreelancer, f => f.Random.Int(1, 100))
+            .RuleFor(p => p.TotalCost, f => f.Random.Decimal(1000, 10000));
+
+        public static User CreateFakeUserClient() => _userFakerClient.Generate();
+
+        public static User CreateFakeUserFreelancer() => _userFakerFreelancer.Generate();
 
         public static Project CreateFakeProject() => _projectFaker.Generate();
 
