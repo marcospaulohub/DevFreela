0503b03d3600adc9e629273da4be3ca7b62f9243
Marcos Paulo <marcos.poo@gmail.com>
2025-03-01 21:54:08 -0300

    feat: Adiciona Handler e Query para Login.

---
+++ DevFreela.Application/Queries/Users/Login/LoginHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using DevFreela.Application.Models;
+using DevFreela.Core.Messages.UserMessages;
+using DevFreela.Core.Repositories;
+using DevFreela.Infrastructure.Auth;
+
+namespace DevFreela.Application.Queries.Users.Login
+{
+    public class LoginHandler : IRequestHandler<LoginQuery, ResultViewModel<LoginViewModel>>
+    {
+        private readonly IUserRepository _repository;
+        private readonly IAuthService _authService;
+
+        public LoginHandler(IUserRepository repository, IAuthService authService)
+        {
+            _repository = repository;
+            _authService = authService;
+        }
+
+        public async Task<ResultViewModel<LoginViewModel>> Handle(LoginQuery request, CancellationToken cancellationToken)
+        {
+            var hash = _authService.ComputeHash(request.Password);
+
+            var user = await _repository.Login(request.Email, hash);
+
+            if (user is null)
+            {
+                return ResultViewModel<LoginViewModel>.Error(UserMsgs.GetUserNotExist());
+            }
+
+            var token = _authService.GenerateToken(user.Email, user.Role);
+
+            var model = LoginViewModel.FromEntity(token);
+
+            return ResultViewModel<LoginViewModel>.Success(model);
+        }
+    }
+}
---
+++ DevFreela.Application/Queries/Users/Login/LoginQuery.cs
@@ -0,0 +1,17 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+
+namespace DevFreela.Application.Queries.Users.Login
+{
+    public class LoginQuery : IRequest<ResultViewModel<LoginViewModel>>
+    {
+        public LoginQuery(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
