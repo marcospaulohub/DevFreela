f0032dbfabd2eb8eca4e7f9df9d42bb354815ee2
Marcos Paulo <marcos.poo@gmail.com>
2025-03-01 21:57:57 -0300

    feat: Adiciona e utiliza o IAuthService para gerar hash para o user.

--- DevFreela.Application/Commands/Users/InsertUser/InsertUserHandler.cs
+++ DevFreela.Application/Commands/Users/InsertUser/InsertUserHandler.cs
@@ -3,21 +3,26 @@ using System.Threading.Tasks;
 using MediatR;
 using DevFreela.Application.Models;
 using DevFreela.Core.Repositories;
+using DevFreela.Infrastructure.Auth;
 
 namespace DevFreela.Application.Commands.Users.InsertUser
 {
     public class InsertUserHandler : IRequestHandler<InsertUserCommand, ResultViewModel<int>>
     {
         private readonly IUserRepository _repository;
+        private readonly IAuthService _authService;
 
-        public InsertUserHandler(IUserRepository repository)
+        public InsertUserHandler(IUserRepository repository, IAuthService authService)
         {
             _repository = repository;
+            _authService = authService;
         }
 
         public async Task<ResultViewModel<int>> Handle(InsertUserCommand request, CancellationToken cancellationToken)
         {
-            var user = request.ToEntity();
+            var hash = _authService.ComputeHash(request.Password);
+
+            var user = request.ToEntity(hash);
 
             await _repository.Add(user);
 
