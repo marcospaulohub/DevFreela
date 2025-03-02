d04404e9f7cd34e2b09e825a8bfac4cc9f9c14a4
Marcos Paulo <marcos.poo@gmail.com>
2025-03-01 21:29:27 -0300

    feat: Adiciona validação para os novos atributos da classe User.

--- DevFreela.Application/Validators/UserValidator/CreateUserValidator.cs
+++ DevFreela.Application/Validators/UserValidator/CreateUserValidator.cs
@@ -28,6 +28,18 @@ namespace DevFreela.Application.Validators.UserValidator
                     .WithMessage(UserMsgs.GetBirthDateNotEmpty())
                 .Must(d => d < DateTime.Now.AddYears(-18))
                     .WithMessage(UserMsgs.GetBirtDateMinAge());
+
+            RuleFor(u => u.Password)
+                .NotEmpty()
+                    .WithMessage(UserMsgs.GetPasswordNotEmpty())
+                .MaximumLength(20)
+                    .WithMessage(UserMsgs.GetPasswordMaxLength());
+
+            RuleFor(u => u.Role)
+                .NotEmpty()
+                    .WithMessage(UserMsgs.GetRoleNotEmpty())
+                .MaximumLength(20)
+                    .WithMessage(UserMsgs.GetRoleMaxLength());
         }
     }
 }
