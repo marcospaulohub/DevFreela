34b8cdcc79e5d0b9e0eddc58f626a005c2996b26
Marcos Paulo <marcos.poo@gmail.com>
2025-03-05 21:10:21 -0300

    feat: Adiciona mais um método de geração de dados fakes.

--- DevFreela.UnitTests/Fakes/FakeDataHelper.cs
+++ DevFreela.UnitTests/Fakes/FakeDataHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Bogus;
 using DevFreela.Application.Commands.Projects.DeleteProject;
+using DevFreela.Application.Commands.Projects.InsertComment;
 using DevFreela.Application.Commands.Projects.InsertProject;
 using DevFreela.Core.Entities;
 
@@ -28,12 +29,20 @@ namespace DevFreela.UnitTests.Fakes
                 f.Random.Int(1, 100),
                 f.Random.Decimal(1000, 10000)));
 
+
         private static readonly Faker<InsertProjectCommand> _insertProjectCommandFaker = new Faker<InsertProjectCommand>()
             .RuleFor(c => c.Title, f => f.Commerce.ProductName())
             .RuleFor(c => c.Description, f => f.Lorem.Sentence())
             .RuleFor(c => c.IdClient, f => f.Random.Int(1, 100))
             .RuleFor(c => c.IdFreelancer, f => f.Random.Int(1, 100))
             .RuleFor(c => c.TotalCost, f => f.Random.Decimal(1000, 10000));
+
+
+        private static readonly Faker<InsertCommentCommand> _InsertCommentCommandFaker = new Faker<InsertCommentCommand>()
+            .RuleFor(c => c.Content, f => f.Commerce.ProductName())
+            .RuleFor(c => c.IdProject, f => f.Random.Int(2, 100))
+            .RuleFor(c => c.IdUser, f => f.Random.Int(3, 100));
+
 
         private static readonly Faker<User> _userFakerClient = new Faker<User>()
             .RuleFor(u => u.Id, f => f.Random.Int(1, 1000))
@@ -43,6 +52,7 @@ namespace DevFreela.UnitTests.Fakes
             .RuleFor(u => u.Password, f => f.Internet.Password(10))
             .RuleFor(u => u.Role, "client");
 
+        
         private static readonly Faker<User> _userFakerFreelancer = new Faker<User>()
             .RuleFor(u => u.Id, f => f.Random.Int(1, 1000))
             .RuleFor(u => u.FullName, f => f.Name.FullName())
@@ -51,10 +61,12 @@ namespace DevFreela.UnitTests.Fakes
             .RuleFor(u => u.Password, f => f.Internet.Password(10))
             .RuleFor(u => u.Role, "freelancer");
 
+
         private static readonly Faker<Skill> _skillFaker = new Faker<Skill>()
             .RuleFor(s => s.Id, f => f.Random.Int(1, 1000))
             .RuleFor(s => s.Description, f => f.Commerce.ProductName());
 
+        
         private static readonly Faker<Project> _projectFaker = new Faker<Project>()
             .RuleFor(p => p.Id, f => f.Random.Int(1, 1000))
             .RuleFor(p => p.Title, f => f.Commerce.ProductName())
@@ -63,18 +75,22 @@ namespace DevFreela.UnitTests.Fakes
             .RuleFor(p => p.IdFreelancer, f => f.Random.Int(1, 100))
             .RuleFor(p => p.TotalCost, f => f.Random.Decimal(1000, 10000));
 
-        public static User CreateFakeUserClient() => _userFakerClient.Generate();
 
+        public static Project CreateFakeProject() => _projectFaker.Generate();
+        public static List<Project> CreateFakeProjectList() => _projectFaker.Generate(5);
+
+
+        public static User CreateFakeUserClient() => _userFakerClient.Generate();
         public static User CreateFakeUserFreelancer() => _userFakerFreelancer.Generate();
 
         public static Skill CreateFakeSkill() => _skillFaker.Generate();
 
-        public static Project CreateFakeProject() => _projectFaker.Generate();
-
-        public static List<Project> CreateFakeProjectList() => _projectFaker.Generate(5);
 
         public static InsertProjectCommand CreateFakeInsertProjectCommand()
             => _insertProjectCommandFaker.Generate();
+
+        public static InsertCommentCommand CreateFakeInsertCommentCommand()
+            => _InsertCommentCommandFaker.Generate();
 
         // Precisamos realmente disso?
         public static DeleteProjectCommand CreateFakeDeleteProjectCommand(int id)
